16d9975eb245c34974de829a161318d8b4c77ca8
Nikiteo <nzhigiley@yandex.ru>
2020-06-16 21:22:26 +0300

    Add homeworks tasks

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,30 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    if (isNaN(percent)) {
+        console.log(`Параметр "Процентная ставка" содержит неправильное значение ${percent}`);
+        return;
+    }
+    else if (isNaN(contribution)) {
+        console.log(`Параметр "Начальный взнос" содержит неправильное значение ${contribution}`);
+        return;
+    }
+    else if (isNaN(amount)) {
+        console.log(`Параметр "Общая стоимость" содержит неправильное значение ${amount}`);
+        return;
+    }
+    else {
+        let body = amount - contribution;
+        let creditTerm =  ((date.getFullYear() - new Date().getFullYear()) * 12) + (date.getMonth() - new Date().getMonth());
+        let valueForAmount = (percent/100)/12;
+        let paidMonthly = body * (valueForAmount + valueForAmount / (((1 + valueForAmount) ** creditTerm) - 1));
+        let totalAmount = parseFloat(creditTerm * paidMonthly).toFixed(2);
+        return totalAmount;
+    }
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    return !name || name == "null" || name == "undefined" || name == " " ? console.log('Привет, мир! Меня зовут Аноним') : console.log(`Привет, мир! Меня зовут ${name}`);
 }
+
